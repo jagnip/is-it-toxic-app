fbaba7350914bb63d568036f31de4be4fd53863c
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-07 13:21:55 +0000

    Styled emojis

--- src/app/_components/SearchResults.tsx
+++ src/app/_components/SearchResults.tsx
@@ -1,17 +1,28 @@
 import { PLANTS } from "../_data/PLANTS";
 
 export default function SearchResults() {
+  const toxicToEmojis: Record<string, string> = {
+    dog: "🐶",
+    cat: "🐱",
+    horse: "🐴",
+  };
+
   return (
-    <div className="h-[50vh] overflow-y-auto p-4 scrollbar-hide">
+    <div className="h-[40vh] overflow-y-auto p-4 scrollbar-hide">
       <ul>
         {Object.keys(PLANTS).map((key) => {
           const plant = PLANTS[key];
           return (
-            <li className="flex justify-between p-2" key={key}>
+            <li className="flex justify-between items-center p-2" key={key}>
               <div className="flex flex-col">
-                <span>{plant.name}</span> <span className="text-gray-500">{plant.scientificName}</span>
+                <span>{plant.name}</span>{" "}
+                <span className="text-gray-500">{plant.scientificName}</span>
               </div>
-              <span>{plant.toxicTo.join(", ")}</span>
+              <div className="bg-red-200 rounded-2xl flex px-1 py-0.5">
+                {plant.toxicTo.map((animal) => (
+                  <div key={animal}>{toxicToEmojis[animal]}</div>
+                ))}
+              </div>
             </li>
           );
         })}
