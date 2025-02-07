5aa363c620fe4972362cd75e26c9031ccfd6ea89
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-07 11:00:16 +0000

    Added PlantDetails

--- src/app/_components/PlantDetails.tsx
+++ src/app/_components/PlantDetails.tsx
@@ -1,9 +1,48 @@
 import { PLANTS } from "../_data/PLANTS";
 
 export default function PlantDetails() {
+  const plant = PLANTS["adam-and-eve arum maculatum"];
   return (
     <div>
-      <h1>PlantDetails</h1>
+      <button>←</button>
+      <a href={plant.link} target="_blank" rel="noopener noreferrer">
+        ASPCA Website
+      </a>
+      <h1>{plant.name}</h1>
+      <h2>{plant.scientificName}</h2>
+
+      <dl>
+        {plant.family && (
+          <div>
+            <dt>Family</dt>
+            <dd>{plant.family}</dd>
+          </div>
+        )}{" "}
+        {plant.commonNames && (
+          <div>
+            <dt>Common names</dt>
+            <dd>{plant.commonNames.join(",  ")}</dd>
+          </div>
+        )}
+        {plant.toxicTo && (
+          <div>
+            <dt>Toxic to</dt>
+            <dd>{plant.toxicTo.join(",  ")}</dd>
+          </div>
+        )}{" "}
+        {plant.toxicPrinciples && (
+          <div>
+            <dt>Toxic principles</dt>
+            <dd>{plant.toxicPrinciples}</dd>
+          </div>
+        )}
+        {plant.clinicalSigns && (
+          <div>
+            <dt>Clinical signs</dt>
+            <dd>{plant.clinicalSigns}</dd>
+          </div>
+        )}
+      </dl>
     </div>
   );
 }
