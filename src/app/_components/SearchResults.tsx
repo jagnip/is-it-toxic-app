3203e4aab91cc95bae6076e9f0254df0a427f526
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-07 12:58:26 +0000

    Styled search results

--- src/app/_components/SearchResults.tsx
+++ src/app/_components/SearchResults.tsx
@@ -2,13 +2,15 @@ import { PLANTS } from "../_data/PLANTS";
 
 export default function SearchResults() {
   return (
-    <div className="h-[50vh] overflow-y-auto">
+    <div className="h-[50vh] overflow-y-auto p-4 scrollbar-hide">
       <ul>
         {Object.keys(PLANTS).map((key) => {
           const plant = PLANTS[key];
           return (
-            <li key={key}>
-              <span>{plant.name}</span> <span>{plant.scientificName}</span>{" "}
+            <li className="flex justify-between p-2" key={key}>
+              <div className="flex flex-col">
+                <span>{plant.name}</span> <span className="text-gray-500">{plant.scientificName}</span>
+              </div>
               <span>{plant.toxicTo.join(", ")}</span>
             </li>
           );
