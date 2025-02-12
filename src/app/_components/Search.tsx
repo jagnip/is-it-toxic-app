3c99d6b43b731b1eb61c2800288a3f416f7b9240
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-12 13:10:28 +0000

    Lifted SearchInput state to Search

--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -1,10 +1,19 @@
 import SearchInput from "./SearchInput";
 import SearchResults from "./SearchResults";
+import { useState } from "react";
 
 export default function Search() {
+  const [searchItem, setSearchItem] = useState<string>("");
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const searchItem = e.target.value;
+    setSearchItem(searchItem);
+    console.log(searchItem);
+  }
+
   return (
     <div className="w-[390px] md:w-[590px] shadow-[0_10px_15px_-3px_rgba(223,233,203,0.5),0_4px_6px_-4px_rgba(223,233,203,0.5)] rounded-xl max-w-xl bg-white border border-gray-100">
-      <SearchInput />
+      <SearchInput handleChange={handleInputChange} searchItem={searchItem} />
       <SearchResults />
     </div>
   );
--- src/app/_components/SearchInput.tsx
+++ src/app/_components/SearchInput.tsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
+interface SearchInputProps {
+  searchItem: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-export default function SearchInput() {
-  const [searchItem, setSearchItem] = useState<string>("");
-
-  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const searchItem = e.target.value;
-    setSearchItem(searchItem);
-    console.log(searchItem)
-  }
-
+export default function SearchInput({
+  searchItem,
+  handleChange,
+}: SearchInputProps) {
   return (
     <input
       type="text"
       placeholder="Search for toxic plants..."
       value={searchItem}
-      onChange={handleInputChange}
+      onChange={handleChange}
       className="text-xl p-4 rounded-t-xl border-b w-full border-gray-100 outline-none focus:ring-0 focus:border-transparent"
     />
   );
