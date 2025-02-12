7b98766e8730f9723f05bb11efcaeef4878428a4
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-12 12:59:21 +0000

    Added input state

--- src/app/_components/SearchInput.tsx
+++ src/app/_components/SearchInput.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export default function SearchInput() {
+  const [searchItem, setSearchItem] = useState<string>("");
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const searchItem = e.target.value;
+    setSearchItem(searchItem);
+    console.log(searchItem)
+  }
+
   return (
     <input
       type="text"
       placeholder="Search for toxic plants..."
+      value={searchItem}
+      onChange={handleInputChange}
       className="text-xl p-4 rounded-t-xl border-b w-full border-gray-100 outline-none focus:ring-0 focus:border-transparent"
     />
   );
