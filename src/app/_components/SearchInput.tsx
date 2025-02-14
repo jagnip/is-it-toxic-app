77d77121f92924e419f3220b085ca160de229413
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-14 08:29:19 +0000

    installed json-server

--- src/app/_components/SearchInput.tsx
+++ src/app/_components/SearchInput.tsx
@@ -1,3 +1,4 @@
+import { usePathname } from "next/navigation";
 import { useRef, useEffect } from "react";
 
 interface SearchInputProps {
@@ -12,19 +13,13 @@ export default function SearchInput({
   setIsFocused,
 }: SearchInputProps) {
   const inputRef = useRef<HTMLInputElement>(null);
-  // const pathname = usePathname();
+  const pathname = usePathname();
 
-  // useEffect(() => {
-  //   if (inputRef.current ) {
-  //     inputRef.current.focus();
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //     if (inputRef.current && (pathname === "/")) {
-  //       inputRef.current.focus();
-  //     }
-  // }, [pathname]);
+  useEffect(() => {
+      if (inputRef.current && (pathname === "/")) {
+        inputRef.current.focus();
+      }
+  }, [pathname]);
 
   return (
     <input
