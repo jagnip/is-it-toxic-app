671cf30f09656f91073d038317fe5f276661d70c
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-18 09:24:50 +0000

    Updated Layout with conditional rendering

--- src/app/_components/Layout.tsx
+++ src/app/_components/Layout.tsx
@@ -4,11 +4,15 @@ import Header from "./Header";
 import { Plants, Plant } from "@/types";
 import { useContext } from "react";
 import PlantsContext from "../_context/PlantsContext";
+import { usePathname } from "next/navigation";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const plants: Plants = useContext(PlantsContext);
+
+  const pathname = usePathname();
+  const isPlantPage = pathname.startsWith("/plants/");
 
   return (
     <div className="flex flex-col xl:flex-row xl:max-w-[100vw] xl:p-8 items-center justify-center xl:h-[100vh]">
@@ -16,8 +20,9 @@ export default function Layout({
         <Header />
         <Search plants={plants} />
       </div>
-
-      <div className="m-4 xl:max-w-[600px] xl:flex-[4]">{children}</div>
+      {isPlantPage && (
+        <div className="m-4 xl:max-w-[600px] xl:flex-[4]">{children}</div>
+      )}
     </div>
   );
 }
