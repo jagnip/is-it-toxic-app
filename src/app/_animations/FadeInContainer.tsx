5a85f2d7bc4752e2c6e4ad80ca76cb9ab4e09f74
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-02-19 10:52:45 +0000

    Tuned in entry animation

--- src/app/_animations/FadeInContainer.tsx
+++ src/app/_animations/FadeInContainer.tsx
@@ -1,18 +1,17 @@
 import { motion } from "framer-motion";
 
-export default function ScaleInContainer({
+export default function FadeInDownContainer({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.9 }}
-      animate={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{
-        duration: 0.4,
+        duration: 0.5,
         ease: "easeOut",
-        opacity: { duration: 0.05 },
       }}
     >
       {children}
--- src/app/_animations/FadeInDownContainer.tsx
+++ src/app/_animations/FadeInDownContainer.tsx
@@ -10,7 +10,7 @@ export default function FadeInDownContainer({
       initial={{ opacity: 0, y: 100, scale: 0.8 }}
       animate={{ opacity: 1, y: 0, scale: 1 }}
       transition={{
-        duration: 0.5,
+        duration: 0.4,
         ease: "easeOut",
         opacity: { delay: 0.05, duration: 0.1 },
       }}
